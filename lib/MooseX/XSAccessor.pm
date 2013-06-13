3090984be45b8fb64341375a5dd559680379a3c8
Toby Inkster <mail@tobyinkster.co.uk>
2013-06-13 23:24:34 +0100

    initial commit

---
+++ lib/MooseX/XSAccessor.pm
@@ -0,0 +1,59 @@
+package MooseX::XSAccessor;
+
+use 5.010;
+use constant { false => 0, true => 1 };
+use strict;
+use warnings;
+use utf8;
+
+BEGIN {
+	$MooseX::XSAccessor::AUTHORITY = 'cpan:TOBYINK';
+	$MooseX::XSAccessor::VERSION   = '0.001';
+}
+
+
+
+
+# Your code goes here
+
+true;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+MooseX::XSAccessor - use Class::XSAccessor to speed up Moose accessors
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=MooseX-XSAccessor>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
