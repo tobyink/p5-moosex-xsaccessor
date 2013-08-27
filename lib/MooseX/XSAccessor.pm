777a3d93f302810ce7278009123ab6c0bb6e9248
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-27 09:21:01 +0100

    bump version; changelog

--- lib/MooseX/XSAccessor.pm
+++ lib/MooseX/XSAccessor.pm
@@ -10,7 +10,7 @@ use Scalar::Util qw(blessed);
 
 BEGIN {
 	$MooseX::XSAccessor::AUTHORITY = 'cpan:TOBYINK';
-	$MooseX::XSAccessor::VERSION   = '0.004';
+	$MooseX::XSAccessor::VERSION   = '0.005';
 }
 
 our $LVALUE;
